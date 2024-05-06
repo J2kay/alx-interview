8eff6f5aa16afc9f32633d9d5eb22963e8f5a752
Jeffrey Karikari <jeffreyk31k@gmail.com>
2024-05-06 00:55:30 +0000

    task 1 fix

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
-"""
-Determines if all the boxes can be opened.
-"""
+"""determines if all the boxes can be opened."""
 
-def can_unlock_all(boxes):
-	"""lockboxes checker code"""
-    opened_boxes = {0}
-    unused_keys = set()
 
-    for box_number, box in enumerate(boxes):
-        if box_number in opened_boxes:
-            continue
+def canUnlockAll(boxes):
+    """determines if all the boxes can be opened.
+    """
 
-        if any(key in opened_boxes for key in box):
-            unused_keys.update(box)
-
-        opened_boxes.add(box_number)
-
-    return len(opened_boxes) == len(boxes)
+    keys = [0]
+    for key in keys:
+        for boxKey in boxes[key]:
+            if boxKey not in keys and boxKey < len(boxes):
+                keys.append(boxKey)
+    if len(keys) == len(boxes):
+        return True
+    return False
