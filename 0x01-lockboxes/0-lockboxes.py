2f04762d275bc38182fb46279c8ce953c6186e11
Jeffrey Karikari <jeffreyk31k@gmail.com>
2024-05-06 00:47:42 +0000

    taks 1

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Determines if all the boxes can be opened.
+"""
+
+def can_unlock_all(boxes):
+    opened_boxes = {0}
+    unused_keys = set()
+
+    for box_number, box in enumerate(boxes):
+        if box_number in opened_boxes:
+            continue
+
+        if any(key in opened_boxes for key in box):
+            unused_keys.update(box)
+
+        opened_boxes.add(box_number)
+
+    return len(opened_boxes) == len(boxes)
